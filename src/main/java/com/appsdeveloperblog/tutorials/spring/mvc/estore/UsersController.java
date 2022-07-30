8117cdf014669b9a368ae750b51c47676a14d883
johannpa <johann_paque@hotmail.fr>
2022-07-30 21:09:16 +0200

    An example of what you can do

--- src/main/java/com/appsdeveloperblog/tutorials/spring/mvc/estore/UsersController.java
+++ src/main/java/com/appsdeveloperblog/tutorials/spring/mvc/estore/UsersController.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.tutorials.spring.mvc.estore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +15,12 @@ public class UsersController {
         ModelAndView modelAndView = new ModelAndView("album");
         modelAndView.addObject("userId", userId);
         modelAndView.addObject("albumId", albumId);
+        return modelAndView;
+    }
+
+    @GetMapping(path = "/users")
+    public ModelAndView getUsers(@RequestParam(name = "limit", required = false) int limit) {
+        ModelAndView modelAndView = new ModelAndView("users");
         return modelAndView;
     }
 }
