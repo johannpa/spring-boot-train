e08444d56ce63c0e5ea12fed416fadde690f9a13
johannpa <johann_paque@hotmail.fr>
2022-07-30 20:58:21 +0200

    training is the key ;)

---
+++ src/main/java/com/appsdeveloperblog/tutorials/spring/mvc/estore/UsersController.java
@@ -0,0 +1,19 @@
+package com.appsdeveloperblog.tutorials.spring.mvc.estore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UsersController {
+
+    @GetMapping(path = "/users/{userId}/albums/{albumId}")
+    public ModelAndView getUser(@PathVariable("userId") String userId,
+                                @PathVariable("albumId") String albumId) {
+        ModelAndView modelAndView = new ModelAndView("album");
+        modelAndView.addObject("userId", userId);
+        modelAndView.addObject("albumId", albumId);
+        return modelAndView;
+    }
+}
